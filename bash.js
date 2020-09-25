fdcf1d3629712aba5e428fe45a9e032b6ed6a751
Seong Jin Kim <seongjinkimsjk@gmail.com>
2020-09-25 12:49:11 -0400

    Process Global DONE

---
+++ bash.js
@@ -0,0 +1,18 @@
+// process.stdout.write("prompt > ");
+
+// process.stdin.on("data", (data) => {
+//   const cmd = data.toString().trim();
+//   process.stdout.write("You typed: " + cmd);
+//   process.stdout.write("\nprompt > ");
+// });
+
+process.stdout.write("prompt > ");
+
+process.stdin.on("data", (data) => {
+  const cmd = data.toString().trim();
+
+  if (cmd === "pwd") {
+    process.stdout.write(process.cwd());
+  }
+  process.stdout.write("\nprompt > ");
+});
